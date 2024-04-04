e500fffa13379dbb1605a9c260a85b3b978fad2b
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2024-04-04 09:12:32 -0500

    first commit

---
+++ tests/test_pipelines_rclinuxedr.py
@@ -0,0 +1,5 @@
+import pytest
+from sigma.backends.rclinuxedr import rclinuxedrBackend
+from sigma.pipelines.rclinuxedr import # TODO: import pipeline functions
+
+# TODO: import tests for all implemented pipelines and contained transformations
