9a19004a47c5f8e179a21377133aaf9b341a14d1
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2024-04-07 16:40:07 -0500

    update unit tests

--- tests/test_pipelines_rclinuxedr.py
+++ tests/test_pipelines_rclinuxedr.py
@@ -48,8 +48,8 @@ def test_rclinuxedr_process_creation_mapping(test_backend : TextQueryTestBackend
         """)
     ) == ['event_type_cd="process_start" and process_pid="process_pid_here" and process_path="process_path_here" and process_path="process_path_here" and process_command_line="process_command_line_here" and working_directory="working_directory_here" and (user_name="user_name_and_login_user_name_here" or login_user_name="user_name_and_login_user_name_here") and process_md5="process_md5_here" and process_sha256="sha256_here" and parent_process_pid="parent_process_pid_here" and parent_process_path_name="parent_process_path_here" and parent_process_path="parent_process_path_here"']
 
-def test_rclinuxedr_network_no_direction_mapping(cortexxdr_backend : CortexXDRBackend):
-    assert cortexxdr_backend.convert(
+def test_rclinuxedr_network_no_direction_mapping(test_backend : TextQueryTestBackend):
+    assert test_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
@@ -99,8 +99,8 @@ def test_rclinuxedr_network_inbound_mapping(test_backend : TextQueryTestBackend)
         """)
     ) == ['event_type_cd="network_connection" and domain="domain_here" and protocol_cd="protocol_here" and (local_ip="ipaddress_value" or remote_ip="ipaddress_value") and local_port="destination_port_titleized" and local_ip="destination_ip_titleized" and remote_ip="source_ip_titleized" and remote_port="source_port_titleized" and remote_port="source_port_titleized_short" and local_port="destination_port_titleized_short" and local_ip_type="destination_ip_type_here" and remote_ip_type="source_ip_type_here" and direction_cd="inbound"']
 
-def test_rclinuxedr_network_outbound_mapping(cortexxdr_backend : CortexXDRBackend):
-    assert cortexxdr_backend.convert(
+def test_rclinuxedr_network_outbound_mapping(test_backend : TextQueryTestBackend):
+    assert test_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
